cec4d19e1bbb03509f037241ad003a508493f4ee
SaraSchoonover <saramschoon@gmail.com>
2021-01-19 19:43:04 -0600

    working with arrays

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("arrays-loops");
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+// console.log(students.length);
+
+// ACCESSING ITEMS IN ARRAY
+
+// console.log(students[1]); // Jackie
+// console.log(students[students.length - 1]); // Nathan
+
+students[0] = 'Matthew';
+// console.log(students);
+
+const valuePrinter = (array, index) => {
+  return array[index];
+}
+ console.log(valuePrinter(students, 2));
+
+ // write a function that takes an array of something and tells you if 
+ //the name 'greg' is in that array
+ 
+// const boys = ['Greg', 'Kyle', 'Bob'];
+
+const whereIsGreg = (array) => {
+  return array.includes('Greg');
+};
+console.log(whereIsGreg(students));
